76c77152f43c501b839a6b526c4447ea38d4d345
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-09-25 21:03:55 -0300

    retornando-ao-index-original

--- javascript/script.js
+++ javascript/script.js
@@ -1,114 +1,19 @@
-function mouseSair() {
-    let elementoHome = document.getElementById("link-home");
-    elementoHome.classList.add("mouseSaiu");
-    setTimeout(() => {
-        elementoHome.classList.remove("mouseSaiu");
-    }, 300);
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
+const nav = document.getElementById('nav');
+
+nav.classList.toggle('active');
+
+const active = nav.classList.contains('active');
+
+event.currentTarget.setAttribute('aria-expanded', active);
+if (active) event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
 }
 
-document.getElementById("link-home").addEventListener("mouseleave", mouseSair);
-
-function mouseSairBelgica() {
-    let elementoBelgica = document.getElementById("link-belgica");
-    elementoBelgica.classList.add("saiuBelgica");
-    setTimeout(() => {
-        elementoBelgica.classList.remove("saiuBelgica");
-    }, 300)
-}
-
-document.getElementById("link-belgica").addEventListener("mouseleave", mouseSairBelgica);
-
-function sairCongo() {
-    let elemento = document.getElementById("link-congo");
-    elemento.classList.add("saiuCongo");
-
-    setTimeout(() => {
-        elemento.classList.remove("saiuCongo");
-    }, 300);
-}
-
-document.getElementById("link-congo").addEventListener("mouseleave", sairCongo);
-
-function sairCoreia() {
-    let elementoCoreia = document.getElementById("link-coreia");
-    elementoCoreia.classList.add("saiuCoreia");
-    setTimeout(() => {
-        elementoCoreia.classList.remove("saiuCoreia");
-    }, 300);
-}
-
-document.getElementById("link-coreia").addEventListener("mouseleave", sairCoreia);
-
-function sairCuba() {
-    let elementoCuba = document.getElementById("link-cuba");
-    elementoCuba.classList.add("saiuCuba");
-    setTimeout(() => {
-        elementoCuba.classList.remove("saiuCuba");
-    }, 300);
-}
-
-document.getElementById("link-cuba").addEventListener("mouseleave", sairCuba);
-
-function sairEua() {
-    let elementoEua = document.getElementById("link-eua");
-    elementoEua.classList.add("saiuEua");
-    setTimeout(() => {
-        elementoEua.classList.remove("saiuEua");
-    }, 300);
-}
-
-document.getElementById("link-eua").addEventListener("mouseleave", sairEua);
-
-function sairIsrael() {
-    let elementoIsrael = document.getElementById("link-israel");
-    elementoIsrael.classList.add("saiuIsrael");
-    setTimeout(() =>  {
-        elementoIsrael.classList.remove("saiuIsrael");
-    }, 300);
-}
-
-document.getElementById("link-israel").addEventListener("mouseleave", sairIsrael);
-
-function sairLaos() {
-    let elementoLaos = document.getElementById("link-laos");
-    elementoLaos.classList.add("saiuLaos");
-    setTimeout(() => {
-        elementoLaos.classList.remove("saiuLaos");
-    }, 300);
-}
-
-document.getElementById("link-laos").addEventListener("mouseleave", sairLaos);
-
-function sairRussia() {
-    let elementoRussia = document.getElementById("link-russia");
-    elementoRussia.classList.add("saiuRussia");
-    setTimeout(() => {
-        elementoRussia.classList.remove("saiuRussia");
-    }, 300);
-}
-
-document.getElementById("link-russia").addEventListener("mouseleave", sairRussia);
-
-
-function sairContato() {
-    let elementoContato = document.getElementById("link-contato");
-    elementoContato.classList.add("saiuContato");
-
-    setTimeout(() => {
-        elementoContato.classList.remove("saiuContato");
-    }, 300);
-}
-
-document.getElementById("link-contato").addEventListener("mouseleave", sairContato);
-
-function sairSobre() {
-    let elementoSobre = document.getElementById("link-sobre");
-
-    elementoSobre.classList.add("saiuSobre");
-
-    setTimeout(() => {
-        elementoSobre.classList.remove("saiuSobre");
-    }, 300);
-}
-
-document.getElementById("link-sobre").addEventListener("mouseleave", sairSobre);
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
